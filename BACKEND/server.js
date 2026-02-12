c5d52a9a6954806ddce0af61e1d52e014d583961
vrushaliwalve04 <vrushali2004walve@gmail.com>
2026-02-12 23:44:21 +0530

    Fix Express v5 wildcard route

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -38,6 +38,6 @@ app.listen(PORT, () => {
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // React fallback
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
