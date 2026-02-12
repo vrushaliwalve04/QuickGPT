fd4117f819984a7acf923d3d38c5c3069c79b739
vrushaliwalve04 <vrushali2004walve@gmail.com>
2026-02-12 23:33:00 +0530

    Serve React frontend correctly

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -5,8 +5,12 @@ import connectDB from "./configs/db.js";
 import chatRouter from "./routes/chatRoutes.js";
 import UserRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //DATABASE CONNECTION
 connectDB();
@@ -28,4 +32,12 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("SERVER RUNING AT P0RT 3000");
+});
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// React fallback
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
